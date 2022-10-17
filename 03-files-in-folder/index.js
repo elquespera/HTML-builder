0aba7a4e52e903b74455824a6d3c43e1d3438729
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-10-17 15:27:02 +0200

    refactor: console log

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,8 +10,8 @@ const listFiles = async () => {
     if (file.isFile()) {
       const stats = await fs.stat(path.join(__dirname, folder, file.name));
       const size = `${(stats.size / 1024).toFixed(3)}kb`;
-      const [name, ext] = file.name.split('.')
-      console.log("\x1b[35m", name, "\x1b[0m", '-', "\x1b[34m", ext, "\x1b[0m", '-', "\x1b[32m", size, "\x1b[0m");
+      const [name, ext] = file.name.split('.');
+      console.log(`\x1b[35m${name}\x1b[0m - \x1b[34m${ext}\x1b[0m - \x1b[32m${size}\x1b[0m`);
     }
   });
 }
